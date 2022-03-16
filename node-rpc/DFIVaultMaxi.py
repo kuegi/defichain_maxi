604e803fc882c90688c3fb8857691aee66c761b8
kuegi <29012906+kuegi@users.noreply.github.com>
2022-03-16 08:12:22 +0100

    [VaultMaxi.py] updated from private repo

--- node-rpc/DFIVaultMaxi.py
+++ node-rpc/DFIVaultMaxi.py
@@ -3,6 +3,7 @@ import logging
 import sys
 from time import sleep
 
+import traceback
 import utils
 from utils import rpc, waitForTx
 
@@ -25,19 +26,22 @@ logToConsole = True
 logToFile = False
 logId = ""
 
+
 settingsPath = sys.argv[1] if len(sys.argv) > 1 else None
 # read settings
 if settingsPath is not None:
     print("Importing settings from %s" % settingsPath)
     with open(settingsPath) as f:
         settings = json.load(f)
+        if "NODE_URL" in settings:
+            utils.NODE_URL = settings["NODE_URL"]
         utils.NODE_USER = settings['NODE_USER']
         utils.NODE_PASSWORD = settings['NODE_PASSWORD']
         vaultId = settings['vaultId']
         address = settings['address']
-
-        minCollateralRatio = settings['minCollateralRatio']
-        maxCollateralRatio = settings['maxCollateralRatio']
+        if "minCollateralRatio" in settings and "maxCollateralRatio" in settings:
+            minCollateralRatio = settings['minCollateralRatio']
+            maxCollateralRatio = settings['maxCollateralRatio']
         lmPair = settings['lmPair']
         if "minReinvest" in settings:
             minReinvest = settings['minReinvest']
@@ -56,33 +60,45 @@ logger = utils.setup_logger("SafeVault_" + logId, logging.INFO, logToConsole, lo
 utils.LOGGER = logger
 
 lastheight = rpc('getblockcount')
-
 vault = rpc("getvault", [vaultId])
 assets = lmPair.split("-")
 assetA = assets[0]
 assetB = assets[1]
 targetCollateral = (minCollateralRatio + maxCollateralRatio) / 200
-nextPriceBlock = rpc("getloaninfo")["nextPriceBlock"]
+nextPriceBlock = 0
 
 reinvestMsg = " will not reinvest"
 if minReinvest is not None:
     reinvestMsg = f" will reinvest when balance goes above {minReinvest} DFI"
 
-utils.send_telegram(f"starting to monitor {vaultId}")
+if not logToConsole:
+    utils.send_telegram(f"starting to monitor {vaultId}")
 logger.info(f"starting to monitor. with pair {assetA}-{assetB}. Vault currently at {vault['collateralRatio']}, "
             f"will increase LM above {maxCollateralRatio} and decrease below {minCollateralRatio}," + reinvestMsg)
 
+
 try:
     frozen = False
+
     while assetB == "DUSD":
+        lastheight = rpc('getblockcount')
+        vault = rpc("getvault", [vaultId])
+
+        # TODO: only run loop if new block
+
+        collateralRatio = vault['collateralRatio']
+        if nextPriceBlock <= lastheight:
+            nextPriceBlock = rpc("getloaninfo")["nextPriceBlock"]
+            logger.info(
+                f"--next price block: %d currentRatio %d "
+                % (nextPriceBlock, collateralRatio,))
+
         if vault['state'] == 'frozen':
             if not frozen:
-                logger.warn(f"{lastheight} vault halted, waiting for resume")
+                logger.warning(f"{lastheight} vault halted, waiting for resume")
                 utils.send_telegram(f"arb: vault halted")
             frozen = True
             sleep(60)
-            vault = rpc("getvault", [vaultId])
-            lastheight = rpc('getblockcount')
             continue
         if frozen:
             utils.send_telegram(f"arb: vault active again")
@@ -95,12 +111,7 @@ try:
         if vault['state'] == 'inLiquidation':
             logger.warn(f"got liquidated. quitting")
             break
-        collateralRatio = vault['collateralRatio']
-        if nextPriceBlock <= lastheight:
-            nextPriceBlock = rpc("getloaninfo")["nextPriceBlock"]
-            oracle = rpc("getfixedintervalprice", ["DFI/USD"])
-            logger.info(f"--next price block: %d currentRatio %d DFI price %.4f predicted: %.4f  "
-                        % (nextPriceBlock, collateralRatio, oracle['activePrice'], oracle['nextPrice']))
+
 
         if minReinvest is not None:
             dfiBalance = utils.get_balance(address, "DFI")
@@ -109,9 +120,11 @@ try:
                 logger.info(f"{lastheight} - reinvesting rewards {dfiBalance}@DFI in {txId}")
                 waitForTx(txId)
                 logger.info("done")
-
+                vault = rpc("getvault", [vaultId])
+                collateralRatio = vault['collateralRatio']
 
         if 0 < collateralRatio < minCollateralRatio:
+            # tODO: also check next ratio ?
             # reduce exposure
             neededrepay = vault['loanValue'] - (vault['collateralValue'] / targetCollateral)
             pool = utils.get_pool(lmPair)
@@ -124,35 +137,39 @@ try:
             for loan in vault["loanAmounts"]:
                 t = loan.split("@")
                 openLoans.append(t[1])
+
             if lmPair not in account or assetA not in openLoans or assetB not in openLoans:
                 msg = "ERROR: can't withdraw from pool, no tokens left or no loans left"
                 logger.error(msg)
                 utils.send_telegram(msg)
+                sleep(10)  # to not flood
             else:
                 stock_per_token = pool["reserveA"] / pool['totalLiquidity']
                 removeTokens = min(neededStock / stock_per_token, account[lmPair])
-
+                wanteddusd = pool['reserveB/reserveA'] * neededStock
                 logger.info(f"{rpc('getblockcount')} removing liquidity {round(removeTokens, 4)} tokens for "
                             f"{round(neededStock, 3)}@{assetA} , "
-                            f"{round(pool['reserveB/reserveA'] * neededStock, 3)}@{assetB}")
+                            f"{round(wanteddusd, 3)}@{assetB}")
                 txId = rpc("removepoolliquidity",
                            [address, "%.8f@%s" % (removeTokens, lmPair), utils.get_tx_input(address)])
                 waitForTx(txId)
                 account = utils.get_account(address)
+                neededStock = min(neededStock, account[assetA])
+                wanteddusd = min(wanteddusd, account[assetB])
                 logger.info(
                     f"{rpc('getblockcount')} done, paying back "
-                    f"{round(account[assetA], 3)}@{assetA} , {round(account[assetB], 3)}@{assetB}")
+                    f"{round(neededStock, 3)}@{assetA} , {round(wanteddusd, 3)}@{assetB}")
                 data = {"vaultId": vaultId,
                         "from": address,
-                        "amounts": ["%.8f@%s" % (account[assetA], assetA),
-                                    "%.8f@%s" % (account[assetB], assetB)]
+                        "amounts": ["%.8f@%s" % (neededStock, assetA),
+                                    "%.8f@%s" % (wanteddusd, assetB)]
                         }
                 txId = rpc("paybackloan", [data, utils.get_tx_input(address)])
                 waitForTx(txId)
                 logger.info(f"{rpc('getblockcount')} done payback")
                 utils.send_telegram("done reducing exposure")
 
-        elif collateralRatio < 0 or collateralRatio > maxCollateralRatio:
+        elif maxCollateralRatio > 0 and (collateralRatio < 0 or collateralRatio > maxCollateralRatio):
             # increase exposure
             pool = utils.get_pool(lmPair)
             oracle = rpc("getfixedintervalprice", [assetA + "/USD"])
@@ -188,9 +205,9 @@ try:
             logger.info(f"{rpc('getblockcount')} done ")
             utils.send_telegram("done increasing exposure")
 
-        sleep(5)
-        vault = rpc("getvault", [vaultId])
-        lastheight = rpc('getblockcount')
+        sleep(1)
+
 except Exception as e:
-    logger.error("uncaught exception: "+str(e))
+    logger.error("uncaught exception: " + str(e) + "\n" + traceback.format_exc())
     utils.send_telegram("LMVault: Exception in script!")
+    raise e
