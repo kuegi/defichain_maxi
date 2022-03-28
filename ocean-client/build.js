f411a8feb5ba109e4f450e831b5fa8d53b41a2ad
DefiPages <defichainpages@gmail.com>
2022-03-28 19:25:28 +0200

    Create deterministic zip and calc sha256 hash.

--- ocean-client/build.js
+++ ocean-client/build.js
@@ -1,4 +1,8 @@
 const util = require('util')
+const fs = require('fs');
+const zip = require('deterministic-zip');
+const crypto = require('crypto');
+
 const exec = util.promisify(require('child_process').exec)
 
 fileToBuild = process.env.npm_config_file
@@ -20,18 +24,21 @@ function handleExec(res) {
  * Build and ZIP for AWS Lambda Execution
  */
 async function buildLambda(file) {
+  fs.rmSync('./dist', {force: true, recursive: true}); //delete dist before build to ensure no old files exists 
+
   {
     const command = `npx --package @vercel/ncc ncc build ./src/${file}.ts --source-map -o ./dist/${file}`
     const res = await exec(command, { cwd: __dirname })
     handleExec(res)
   }
+  
+  zip(`./dist/${file}`, `./dist/${file}.zip`, {includes: [`*`], cwd: `./dist/${file}`}, (err) => {
+    console.log(`src/${file}.ts -> dist/${file}.zip`)
+    //calc hash
+    const hash = (crypto.createHash('sha256')).update(fs.readFileSync(`./dist/${file}.zip`)).digest('base64');
+    console.log(`sha256 hash: ${hash}`);
+  });
 
-  {
-    const res = await exec(`zip -r -j ./dist/${file}.zip ./dist/${file}/*`)
-    handleExec(res)
-  }
-
-  console.log(`src/${file}.ts -> dist/${file}.zip`)
 }
 
 buildLambda(fileToBuild).catch(
