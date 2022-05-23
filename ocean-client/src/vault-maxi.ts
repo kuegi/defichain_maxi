29204e7eb18d71f02b2a02ad53a2e66fdd0a4c77
kuegi <29012906+kuegi@users.noreply.github.com>
2022-05-23 13:50:07 +0200

    [vaultMaxi] versionnumber

--- ocean-client/src/vault-maxi.ts
+++ ocean-client/src/vault-maxi.ts
@@ -23,7 +23,7 @@ class maxiEvent {
 
 const MIN_TIME_PER_ACTION_MS = 300 * 1000 //min 5 minutes for action. probably only needs 1-2, but safety first?
 
-const VERSION = "v1.1alpha"
+const VERSION = "v1.1"
 
 export async function main(event: maxiEvent, context: any): Promise<Object> {
     console.log("vault maxi " + VERSION)
