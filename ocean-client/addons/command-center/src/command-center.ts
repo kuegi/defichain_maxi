385494c1b58a222c8adcb881db005ed26fbe3bd1
Krysh <krysh.developer@gmail.com>
2022-06-10 17:06:09 +0200

    [command center] add foundation for first addon command center

---
+++ ocean-client/addons/command-center/src/command-center.ts
@@ -0,0 +1,37 @@
+import { Command, Commands } from './commands/command'
+import { Help } from './commands/help'
+import { checkSafetyOf } from './utils/helpers'
+import { Store, StoredSettings } from './utils/store'
+import { Message, Telegram } from './utils/telegram'
+
+const VERSION = "v1.0"
+
+async function execute(messages: Message[], settings: StoredSettings, telegram: Telegram) {
+    for (const message of messages) {
+        let isSafe = checkSafetyOf(message, settings)
+        if (isSafe) {
+            let command: Command | undefined
+            switch (message.command) {
+                case Commands.Help:
+                    command = new Help(telegram)
+                    break
+                default:
+                    console.log("ignore " + message.command)
+            }
+            await command?.execute()
+        }
+    }
+}
+
+export async function main(): Promise<Object> {
+
+    let store = new Store()
+    let settings = await store.fetchSettings()
+
+    const telegram = new Telegram(settings, "[CommandCenter " + VERSION + "]")
+    let messages = await telegram.getMessages()
+    await execute(messages, settings, telegram)
+    await store.updateExecutedMessageId(messages.slice(-1)[0].id)
+
+    return { statusCode: 200 }
+}
