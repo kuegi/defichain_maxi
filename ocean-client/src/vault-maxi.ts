705d680402edd62a878247c91b3058d0c9faf211
kuegi <29012906+kuegi@users.noreply.github.com>
2022-09-12 16:01:31 +0200

    missed ADDRESS_TESTNET

--- ocean-client/src/vault-maxi.ts
+++ ocean-client/src/vault-maxi.ts
@@ -27,6 +27,7 @@ const MIN_TIME_PER_ACTION_MS = 300 * 1000 //min 5 minutes for action. probably o
 
 export const VERSION = "v2.2.1"
 export const DONATION_ADDRESS = "df1qqtlz4uw9w5s4pupwgucv4shl6atqw7xlz2wn07"
+export const DONATION_ADDRESS_TESTNET= "tZ1GuasY57oin5cej1Wp3MA1pAE4y3tmzq"
 export const DONATION_MAX_PERCENTAGE = 50
 
 export async function main(event: maxiEvent, context: any): Promise<Object> {
