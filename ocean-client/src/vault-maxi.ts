1529aa7a91842d98f31aa5326ebc12f3fc735eed
kuegi <29012906+kuegi@users.noreply.github.com>
2022-11-04 12:30:40 +0100

    marked version as beta

--- ocean-client/src/vault-maxi.ts
+++ ocean-client/src/vault-maxi.ts
@@ -25,7 +25,7 @@ class maxiEvent {
 
 const MIN_TIME_PER_ACTION_MS = 300 * 1000 //min 5 minutes for action. probably only needs 1-2, but safety first?
 
-export const VERSION = 'v2.4.0'
+export const VERSION = 'v2.4.0beta'
 export const DONATION_ADDRESS = 'df1qqtlz4uw9w5s4pupwgucv4shl6atqw7xlz2wn07'
 export const DONATION_ADDRESS_TESTNET = 'tZ1GuasY57oin5cej1Wp3MA1pAE4y3tmzq'
 export const DONATION_MAX_PERCENTAGE = 50
