35cccd30033f364928b082ce09daafdc8582ccaf
kuegi <29012906+kuegi@users.noreply.github.com>
2022-12-12 03:38:59 -0500

    reinvest-threshold-override only applies to reinvest, not donation guard (#125)

--- ocean-client/src/programs/lm-reinvest-program.ts
+++ ocean-client/src/programs/lm-reinvest-program.ts
@@ -107,13 +107,10 @@ export class LMReinvestProgram extends CommonProgram {
   }
 
   async checkAndDoReinvest(balances: Map<string, AddressToken>, telegram: Telegram): Promise<boolean> {
-    const maxReinvestThreshold = Math.max(
-      this.getSettings().reinvestThreshold! * 2,
-      +(process.env.VAULTMAXI_MAXREINVEST ?? 40),
-    ) //anything below 20 DFI is considered a "reinvest all the time"
+    const maxReinvestForDonation = Math.max(this.getSettings().reinvestThreshold!, 20) * 2 //anything below 20 DFI is considered a "reinvest all the time"
 
     const result = await checkAndDoReinvest(
-      maxReinvestThreshold,
+      maxReinvestForDonation,
       balances,
       telegram,
       this,
