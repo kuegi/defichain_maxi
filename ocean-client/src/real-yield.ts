05b8e0d4b7cd0be34be6df56d65f551aa6662065
kuegi <29012906+kuegi@users.noreply.github.com>
2023-01-29 21:46:36 +0100

    added 3-step swap calcs

--- ocean-client/src/real-yield.ts
+++ ocean-client/src/real-yield.ts
@@ -19,7 +19,7 @@ import {
   Vout,
 } from '@defichain/jellyfish-transaction/dist'
 import BigNumber from 'bignumber.js'
-import { PoolPairData } from '@defichain/whale-api-client/dist/api/poolpairs'
+import { PoolPairData, PoolSwapData } from '@defichain/whale-api-client/dist/api/poolpairs'
 
 class Ocean {
   public readonly c: WhaleApiClient
@@ -50,6 +50,7 @@ class Ocean {
 class YieldForPool {
   public paidCommission = new BigNumber(0)
   public fee = new BigNumber(0)
+  public usdValue = new BigNumber(0)
   public token: string
 
   constructor(token: string) {
@@ -57,13 +58,34 @@ class YieldForPool {
   }
 
   public toString(): string {
-    return `${this.token}: ${this.paidCommission.toFixed(4)} comm, ${this.fee.toFixed(4)}`
+    return `${this.token}: ${this.paidCommission.toFixed(4)} comm, ${this.fee.toFixed(4)} fee`
   }
+}
+
+async function getSwaps(o: Ocean, id: string, untilBlock: number): Promise<PoolSwapData[]> {
+  let lastResult = await o.c.poolpairs.listPoolSwapsVerbose(id, 20)
+  const pages = [lastResult]
+  let total = 0
+  while (lastResult.hasNext && lastResult[lastResult.length - 1].block.height > untilBlock) {
+    try {
+      lastResult = await o.c.poolpairs.listPoolSwapsVerbose(id, 20, pages[pages.length - 1].nextToken)
+      pages.push(lastResult)
+      total += pages[pages.length - 1].length
+    } catch (e) {
+      break
+    }
+  }
+
+  return pages.flatMap((page) => page as PoolSwapData[])
 }
 
 export async function main(event: any, context: any): Promise<Object> {
   console.log('real yield calculator')
   const o = new Ocean()
+  const startHeight = (await o.c.stats.get()).count.blocks
+  const analysisWindow = 2880
+  const endHeight = startHeight - analysisWindow
+  console.log('starting at block ' + startHeight + ' analysing until ' + endHeight)
   //read all vaults
   const pools = await o.getAll(() => o.c.poolpairs.list(200))
 
@@ -81,9 +103,12 @@ export async function main(event: any, context: any): Promise<Object> {
     }
     const dataB = yields.get(pool.tokenB.symbol)!
 
-    const swaps = await o.getAll(() => o.c.poolpairs.listPoolSwapsVerbose(pool.id, 50), 100)
+    const swaps = await getSwaps(o, pool.id, endHeight)
     console.log('got ' + swaps.length + ' swaps to process')
     for (const swap of swaps) {
+      if (swap.block.height < endHeight) {
+        break
+      }
       let amountA = undefined
       let amountB = undefined
       let AtoB = true
@@ -143,11 +168,114 @@ export async function main(event: any, context: any): Promise<Object> {
           dataA.fee = dataA.fee.plus(amountA.multipliedBy(fee.dividedBy(new BigNumber(1).minus(fee))))
         }
       }
+      if (amountA === undefined && amountB === undefined) {
+        if (swap.to !== undefined && swap.from !== undefined && swap.type !== undefined) {
+          //swap type BUY means token A of pool is bought
+          let poolSymbolFrom = swap.from.symbol + '-' + (swap.type === 'BUY' ? pool.tokenB.symbol : pool.tokenA.symbol)
+          let poolFrom = pools.find((p) => p.symbol === poolSymbolFrom)
+          let fromAtoB = true
+          if (poolFrom === undefined) {
+            poolSymbolFrom = (swap.type === 'BUY' ? pool.tokenB.symbol : pool.tokenA.symbol) + '-' + swap.from.symbol
+            poolFrom = pools.find((p) => p.symbol === poolSymbolFrom)
+            fromAtoB = false
+          }
+          if (poolFrom !== undefined) {
+            let myFrom = new BigNumber(swap.from.amount)
+              .times(1 - +((fromAtoB ? poolFrom.tokenA.fee?.inPct : poolFrom.tokenB.fee?.inPct) ?? 0))
+              .times(fromAtoB ? poolFrom.priceRatio.ba : poolFrom.priceRatio.ab)
+              .times(1 - +((fromAtoB ? poolFrom.tokenB.fee?.outPct : poolFrom.tokenA.fee?.outPct) ?? 0))
+              .times(1 - +poolFrom.commission)
+
+            amountA = swap.type === 'BUY' ? myFrom.times(pool.priceRatio.ab) : myFrom
+            amountB = myFrom.multipliedBy(pool.priceRatio.ba)
+            if (swap.type !== 'BUY') {
+              let fee = new BigNumber(pool.tokenB.fee?.outPct ?? pool.tokenB.fee?.pct ?? 0)
+              dataB.fee = dataB.fee.plus(amountB.multipliedBy(fee.dividedBy(new BigNumber(1).minus(fee))))
+
+              fee = new BigNumber(pool.tokenA.fee?.inPct ?? pool.tokenA.fee?.pct ?? 0)
+              dataA.fee = dataA.fee.plus(amountA.multipliedBy(fee))
+              dataA.paidCommission = dataA.paidCommission.plus(amountA.multipliedBy(pool.commission))
+            } else {
+              let fee = new BigNumber(pool.tokenB.fee?.inPct ?? pool.tokenB.fee?.pct ?? 0)
+              dataB.fee = dataB.fee.plus(amountB.multipliedBy(fee))
+              dataB.paidCommission = dataB.paidCommission.plus(amountB.multipliedBy(pool.commission))
+
+              fee = new BigNumber(pool.tokenA.fee?.outPct ?? pool.tokenA.fee?.pct ?? 0)
+              dataA.fee = dataA.fee.plus(amountA.multipliedBy(fee.dividedBy(new BigNumber(1).minus(fee))))
+            }
+            /*
+              console.log(
+                'got 3 way swap: ' +
+                  JSON.stringify(swap) +
+                  '\n' +
+                  'assuming swap ' +
+                  swap.from.amount +
+                  '@' +
+                  swap.from.symbol +
+                  '->' +
+                  myFrom.toFixed(8) +
+                  '@' +
+                  (swap.type === 'BUY' ? pool.tokenB.symbol : pool.tokenA.symbol) +
+                  '->' +
+                  (swap.type === 'BUY'
+                    ? amountA.toFixed(8) + '@' + pool.tokenA.symbol
+                    : amountB.toFixed(8) + '@' + pool.tokenB.symbol),
+              )
+              //*/
+          } else {
+            console.warn(
+              'unable to find other pools for 3-way swap: ' +
+                swap.from.symbol +
+                '->' +
+                swap.to.symbol +
+                ' in pool ' +
+                pool.symbol +
+                ' type ' +
+                swap.type,
+            )
+          }
+        }
+      }
     }
   }
-  yields.forEach((v, k) => {
-    console.log(v.toString())
+
+  const prices = await o.getAll(() => o.c.prices.list())
+  let totalCommission = new BigNumber(0)
+  let totalFee = new BigNumber(0)
+  prices.forEach((p) => {
+    if (yields.has(p.price.token)) {
+      const data = yields.get(p.price.token)!
+      data.usdValue = data?.fee.plus(data.paidCommission).times(p.price.aggregated.amount)
+      totalCommission = totalCommission.plus(data.paidCommission.times(p.price.aggregated.amount))
+      totalFee = totalFee.plus(data.fee.times(p.price.aggregated.amount))
+    }
   })
+
+  //DUSD needs to be done manually (no oracle price)
+
+  const data = yields.get('DUSD')!
+  data.usdValue = data?.fee.plus(data.paidCommission)
+  totalCommission = totalCommission.plus(data.paidCommission)
+  totalFee = totalFee.plus(data.fee)
+
+  const result = {
+    totalUSD: {
+      commission: totalCommission.decimalPlaces(8).toNumber(),
+      fee: totalFee.decimalPlaces(8).toNumber(),
+    },
+    tokens: Object(),
+  }
+  yields.forEach((v, k) => {
+    result.tokens[v.token] = {
+      commission: v.paidCommission.decimalPlaces(8).toNumber(),
+      fee: v.fee.decimalPlaces(8).toNumber(),
+      usdValue: v.usdValue.decimalPlaces(8).toNumber(),
+    }
+  })
+
+  console.log('total ' + totalCommission.toFixed(2) + '$ comm + ' + totalFee.toFixed(2) + '$ fees')
+
+  console.log(JSON.stringify(result))
 
   return { statusCode: 200 }
 }
